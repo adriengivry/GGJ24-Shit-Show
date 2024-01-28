3dc2f5f99ba1392d64d80bb24e5c7e44c2d27db2
Adrien GIVRY <contact@adrien-givry.com>
2024-01-28 11:20:01 -0500

    Updated throwable speed

--- Assets/Scripts/Gameplay/VFX/Throwable.cs
+++ Assets/Scripts/Gameplay/VFX/Throwable.cs
@@ -4,12 +4,14 @@ public class Throwable : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float m_travelTime = 1.0f;
-    [SerializeField] private AudioSource m_audiosource;
+    [SerializeField] private float m_rotationSpeed = 360.0f;
 
     [Header("References")]
     [SerializeField] private InstancePool m_splashPool;
+    [SerializeField] private AudioSource m_audiosource;
 
     private Vector2 m_destination;
+    private float m_rotationAngle = 0.0f;
     private float m_strength;
 
     public void Throw(Vector2 from, Vector2 to, float strength)
@@ -18,10 +20,15 @@ public class Throwable : MonoBehaviour
         transform.position = new Vector3(from.x, from.y, transform.position.z);
         m_destination = to;
         m_strength = strength;
+        m_rotationAngle = 0.0f;
     }
 
     private void Update()
     {
+        // Rotate the object around the Z-axis
+        m_rotationAngle += m_rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_rotationAngle);
+
         float step = m_strength * Time.deltaTime / m_travelTime; // Calculate the step based on strength and time
 
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(m_destination.x, m_destination.y, transform.position.z), step);
