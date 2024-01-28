0a3d239d8712097b4f26ccceea276f0dcfae6159
Adrien GIVRY <contact@adrien-givry.com>
2024-01-28 10:44:25 -0500

    Added Game Over quotes

--- Assets/Scripts/Gameplay/GameStates/GameOverState.cs
+++ Assets/Scripts/Gameplay/GameStates/GameOverState.cs
@@ -1,22 +1,37 @@
+using AYellowpaper.SerializedCollections;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameOverState : AGameState
 {
     [Header("Settings")]
     [SerializeField] private float m_duration = 5.0f;
     [SerializeField] private string m_nextSceneName = "Main Menu";
-    [SerializeField] private TextMeshProUGUI m_text;
+    [SerializeField] private TextMeshProUGUI m_scoreText;
+    [SerializeField] private TextMeshProUGUI m_quoteText;
 
     [Header("References")]
     [SerializeField] private GameObject m_gameOverUI;
+    [SerializeField] private SerializedDictionary<int, string> m_quotePerScoreMilestone;
 
     public override void OnEnterState(GameManager manager)
     {
         m_gameOverUI.SetActive(true);
-        m_text.text = m_text.text.Replace("{score}", manager.Score.ToString());
+
+        foreach (var milestone in m_quotePerScoreMilestone)
+        {
+            if (manager.Score >= milestone.Key)
+            {
+                // If the score meets or exceeds the milestone, display the corresponding quote
+                m_quoteText.text = milestone.Value;
+                break; // Exit the loop after finding the first milestone that has been reached
+            }
+        }
+
+        m_scoreText.text = m_scoreText.text.Replace("{score}", manager.Score.ToString());
         StartCoroutine(GoToScene(m_nextSceneName, m_duration));
     }
 
