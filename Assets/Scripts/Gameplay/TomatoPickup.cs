ac85305aae637c8d6a8c554f75ea5379242fe5d4
Adrien GIVRY <contact@adrien-givry.com>
2024-01-28 10:26:23 -0500

    Tweaked enemy speed and shooting range refill speed
    - Also added more missed throw locations and adjusted throw destination to center of comedian face
    - made the tomato scale up when it appears
    - Adjusted the tomato respawn cooldown

--- Assets/Scripts/Gameplay/TomatoPickup.cs
+++ Assets/Scripts/Gameplay/TomatoPickup.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class TomatoPickup : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private float m_scalingSpeed = 2.0f;
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime * m_scalingSpeed);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
